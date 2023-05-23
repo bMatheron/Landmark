9fa3aa5c4f9a43d01447c3a5421bc35f22b7a456
bMatheron <bmatheron@icloud.com>
2023-05-23 21:58:46 +0200

    Initial Commit

---
+++ SwiftUI Essentials/SwiftUI_EssentialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_EssentialsApp.swift
+//  SwiftUI Essentials
+//
+//  Created by bMatheron on 23/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_EssentialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
